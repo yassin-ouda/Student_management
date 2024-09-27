aad48c7285653b48845822e265e34220621881cf
yassin-ouda <Yassin.Alaa20@fcai.usc.edu.eg>
2024-09-27 11:28:18 +0300

    Delete Enrollement directory

--- Enrollement/src/main/java/com/EducationSystem/Enrollement/Repository/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.EducationSystem.Enrollement.Repository;
-
-import com.EducationSystem.Enrollement.Model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepo extends JpaRepository<Course, Integer> {
-}
